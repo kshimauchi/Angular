b1f9cf40cef38991701462f3af610d0c8b2b92f4
kshimauchi <kshimauchi@gmail.com>
2020-06-14 23:42:43 -0700

    extracted component

---
+++ angular-material/src/app/material/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material';
+
+const MaterialComponents = [
+  MatButtonModule
+];
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+})                                                                                                                                                                                                                                                                                                                                                                                                                      
+
+export class MaterialModule { }
